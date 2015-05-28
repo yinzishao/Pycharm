ab42e24f508c0986f113d1ed274e5bed3c01a6bf
yinzishao <370953598@qq.com>
2015-05-28 21:42:48 +0800

    finish reuters

--- NewsSpider/NewsSpid_reuters.py
+++ NewsSpider/NewsSpid_reuters.py
@@ -7,31 +7,69 @@ from tool import *
 import re
 from bs4 import BeautifulSoup
 surl = 'http://www.reuters.com/search/news?blob='
+TOTALNUM=0
 
+
+#输入关键词得到链接
 def search(keyword):
-
-
-    keyword=re.sub(r' ','+',keyword)  #转换成url所需格式
-    furl =surl+keyword                #得到最终的url
+    urlset = set()
+    global TOTALNUM
+    keywordchange=re.sub(r' ','+',keyword)  #转换成url所需格式
+    furl =surl+keywordchange                #得到最终的url
     html = geturl_con(furl)
     # print html
     soup = BeautifulSoup(html)
     res =soup.find_all('h3',class_='search-result-title')
-    num =soup.find('span',class_='search-result-count search-result-count-num').string  #得到结果的总数
-    print num
+    TOTALNUM =soup.find('span',class_='search-result-count search-result-count-num').string  #得到结果的总数
+    print TOTALNUM
     # print len(res)
-    uset = set()
+
     for r in res:
-        uset.add(str(r.a['href']))  #提取链接
+        urlset.add(str(r.a['href']))  #提取链接
 
     # print len(uset)
-    return uset
+    #从第二页开始就要从js中获得链接
+    i=2
+    print  TOTALNUM
+    while (((i-1)*10)<int(TOTALNUM)):
+        urlset=urlset|searchjs(keyword,i)
 
-def searchjs(keyword):
+        i=i+1
+        print  TOTALNUM
+        print i
+
+    print len(urlset)
+
+    with open('reuters.txt','w')as r:
+        for url in urlset:
+            r.writelines(url+'\n')
+
+def searchjs(keyword,num):
     headurl = 'http://www.reuters.com/assets/searchArticleLoadMoreJson?blob='+\
               urllib.quote(keyword)\
-              +'&bigOrSmall=big&articleWithBlog=true&sortBy=&dateRange=&numResultsToShow=10&pn=6&callback=addMoreNewsResults'
+              +'&bigOrSmall=big&articleWithBlog=true&sortBy=&dateRange=&numResultsToShow=10&pn='\
+              +urllib.quote(str(num))+'&callback=addMoreNewsResults'
     html =geturl_con(headurl)
-    print html
+    # print html
+    return geturl(html)
+    # soupjs = BeautifulSoup(html)
+
+#得到js中的链接 与Totalnum
+
+def geturl(html):
+    global TOTALNUM
+    numre =re.compile(r'totalResultNumber: (.*?),')
+    urlre =re.compile(r'href: "(.*)",')
+    num =re.search(numre,html)   #得到结果的总数
+    url =re.findall(urlre,html)  #得到链接的set集合
+    urlset = set(url)
+    num = num.group(1)
+
+    if num < TOTALNUM :
+            TOTALNUM = num
+            print TOTALNUM
+            return urlset
+    else:
+            return urlset
 
 search('21st-century maritime silk road')
